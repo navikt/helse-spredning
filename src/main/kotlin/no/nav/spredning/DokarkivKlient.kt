2ea2890670415900fce204b6330b27fb3d97b8c0
Jakob Havstein Eriksen <jakob.havstein.eriksen@nav.no>
2026-06-10 15:12:33 +0200

    Setter journalfoerendeEnhet i request til dokarkiv
    Co-authored-by: Preben Aandahl <preben.aandahl@nav.no>

--- src/main/kotlin/no/nav/spredning/DokarkivKlient.kt
+++ src/main/kotlin/no/nav/spredning/DokarkivKlient.kt
@@ -14,6 +14,7 @@ private val objectMapper = jacksonObjectMapper()
 
 data class OpprettJournalpostRequest(
     val journalposttype: String = "UTGAAENDE",
+    val journalfoerendeEnhet: String = "9999",
     val tema: String = "SYK",
     val tittel: String,
     val avsenderMottaker: AvsenderMottaker,
